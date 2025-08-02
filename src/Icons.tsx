70702d7427b88c5e0e188271a72e7cb7f3d76b9b
root <root@trt.aeza.network>
2025-08-02 09:52:17 +0000

    fd

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -9,7 +9,6 @@ const iconProps = {
   strokeLinejoin: "round" as "round",
 };
 
-// Outlined Icons
 export const HomeIcon = () => <svg {...iconProps} fill="none" stroke="currentColor"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /><polyline points="9 22 9 12 15 12 15 22" /></svg>;
 export const PlusSquareIcon = () => <svg {...iconProps} fill="none" stroke="currentColor"><rect x="3" y="3" width="18" height="18" rx="2" ry="2" /><line x1="12" y1="8" x2="12" y2="16" /><line x1="8" y1="12" x2="16" y2="12" /></svg>;
 export const UserIcon = () => <svg {...iconProps} fill="none" stroke="currentColor"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" /><circle cx="12" cy="7" r="4" /></svg>;
